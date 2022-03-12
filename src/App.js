ba64bb6d0bcd4156e8f3646686cabda2ac9bcc86
Jakub Stohl <jakubstohl@jakub.local>
2022-03-12 20:20:17 +0100

    deployed web 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import backVid from "./background-vid.mp4";
 
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const yourDay = new Date().getDay();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-        <p>How is YOUR {weekday[yourDay]} going?</p>
+      <header class="viewport-header"> 
+      <h1>Hello!</h1>
+        <p>How is <b>YOUR</b> {weekday[yourDay]}?</p>
         <img src={logo} className="App-logo" alt="logo" />
+        <video autoPlay loop muted>         
+          <source src={backVid} type="video/mp4" />
+        </video>
       </header>
     </div>
   );
