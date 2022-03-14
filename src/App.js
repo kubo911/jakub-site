13b92d204bc1ec32a4e393b31009b0a3d70c1119
Jakub Stohl <jakubstohl@jakub.local>
2022-03-14 22:29:12 +0100

    deployed web 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+
 import logo from './logo.svg';
 import './App.css';
 import backVid from "./background-vid.mp4";
+import React, {useState} from "react";
+
 
 
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const yourDay = new Date().getDay();
 
 function App() {
+/////////////////timeFunction
+  let [time, setTime] = useState(new Date().toLocaleTimeString());
+  function getTime() {
+    setTime(new Date().toLocaleTimeString());  }
+  setInterval(getTime, 1000);
+////////////////
+
   return (
     <div className="App">
-      <header class="viewport-header"> 
-      <h1>Hello!</h1>
+      <body class="viewport-body"> 
         <p>How is <b>YOUR</b> {weekday[yourDay]}?</p>
+        <p>What are you doing <b>RIGHT NOW</b>?</p>
+        <p>{time}</p>
+
         <img src={logo} className="App-logo" alt="logo" />
         <video autoPlay loop muted>         
           <source src={backVid} type="video/mp4" />
         </video>
-      </header>
+      </body>
     </div>
   );
 }
